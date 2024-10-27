d9a83117e81b00bb4953714673a224e222b89540
João <joaofernandesxd1@icloud.com>
2024-10-27 13:32:03 -0300

    :sparkles: feat: menu sidedar

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,3 +1,4 @@
+
 /* import styles */
 import './SideBar.css'
 
@@ -6,15 +7,28 @@ import Button from '../button/button';
 
 function Sidebar(){
     return(
-        <div className='container__sidebar'>
+        <aside className='container__sidebar'>
             <div className='container__logo'>
                 <h1>cnt.</h1>
             </div>
             <div className='container__menu'>
-                <Button name='Entrar'></Button>
+                <div className='container__login'>
+                    <Button name='Entrar'></Button>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#home">FILMES</a></li>
+                        <li><a href="#about">SERIES</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#home">RECENTES</a></li>
+                        <li><a href="#about">POPULARES</a></li>
+                        <li><a href="#about">MAIS AVALIADOS</a></li>
+                    </ul>
+                </nav>
 
             </div>
-        </div>
+        </aside>
     )
 }
 
