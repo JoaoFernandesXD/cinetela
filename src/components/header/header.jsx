8dd6f2b9e63a5ed31b90be14bce8536edd0dd225
João Fernandes <joaofernandesxd1@icloud.com>
2024-11-19 13:20:58 -0300

    feat: ajustando nav, ul e li do siderbar e header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,7 +6,13 @@ import './header.css';
 function Header(){
     return(
         <header>
-            
+            <nav>
+                <ul>
+                    <li>Pagina Inicial</li>
+                    <li>Contato</li>
+                    <li>Ajuda</li>
+                </ul>
+            </nav>
         </header>
     )
 }
