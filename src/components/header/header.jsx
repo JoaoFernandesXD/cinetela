72dcfd460c63dad7efb48c6a9e6c36892481d2d3
João Fernandes <joaofernandesxd1@icloud.com>
2024-11-21 14:19:28 -0300

    :sparkless: feat: codificando header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,6 @@
 /* styles */
-
 import './header.css';
-
+import { IoLogoAndroid } from "react-icons/io";
 
 function Header(){
     return(
@@ -12,6 +11,7 @@ function Header(){
                     <li>Contato</li>
                     <li>Ajuda</li>
                 </ul>
+                <div className='button_mobile'>Aplicativo <IoLogoAndroid style={{color:'white', margin:'5px'}}/></div>
             </nav>
         </header>
     )
