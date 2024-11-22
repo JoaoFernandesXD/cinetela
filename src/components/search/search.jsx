6e961d44b1f9379865b333fb2ad2fbcb5e9b265a
João Fernandes <joaofernandesxd1@icloud.com>
2024-11-22 14:29:42 -0300

    feat: finalizando component buscar

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -7,7 +7,9 @@ import './search.css';
 function Search(){
     return(
        <div className='container__search'>
-
+        <p>Busque e voilà, encontre a diversão</p>
+        <div className='button_navegar'>NAVEGAR</div>
+        <input placeholder="Pesquise por filmes, séries ou documentários"></input>
        </div>
     )
 }
