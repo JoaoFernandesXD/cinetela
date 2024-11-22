5b35d56874703d2b895695e3838e9bb37b310db0
João Fernandes <joaofernandesxd1@icloud.com>
2024-11-22 19:12:39 -0300

    feat: creating slide

--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -5,6 +5,10 @@ import './slide.css';
 import ButtonQuality from '../ButtonQuality/ButtonQuality';
 
 
+/* imports icons */
+import { IoMdStar } from "react-icons/io";
+import { FaPlay } from "react-icons/fa";
+
 
 function Slide(){
     return(
@@ -12,7 +16,27 @@ function Slide(){
         <div className='container__slide'>
             <div className='container__info'>
                 <div className='info_movies'>
-                    <ButtonQuality />
+                    <ButtonQuality quality="4K"/>
+                    <p>FILME DESTAQUE</p>
+                    <h2>The Last Of Us</h2>
+                    <div className='container__avaliacao'>
+                        <div className='container_star'>
+                            <IoMdStar style={{color:'var(--color-red)'}} />
+                            <IoMdStar style={{color:'var(--color-red)'}} />
+                            <IoMdStar style={{color:'var(--color-red)'}} />
+                            <IoMdStar style={{color:'var(--color-red)'}} />
+                            <IoMdStar style={{color:'var(--color-gray)'}} />
+                        </div>
+                        <p>4/5</p>
+                        <p>Drama • 1h 35min • 2019</p>
+                    </div>
+                    <div className='Container__text_info'>
+                        <p>Situado duas décadas após a implosão de nossa sociedade, o drama seguirá Joel, um sobrevivente difícil, que é contratado para contrabandear uma garota de 14 anos chamada Ellie...</p>
+                    </div>
+                    <div className='containner__button__slide'>
+                        <button className='watch'><FaPlay />Assistir</button>
+                        <button className='trailer'>Ver Trailer</button>
+                    </div>
                 </div>
             </div>
             <img src='https://image.tmdb.org/t/p/original/uDgy6hyPd82kOHh6I95FLtLnj6p.jpg'></img>
